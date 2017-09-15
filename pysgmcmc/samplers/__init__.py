3a9c513377e75a2a956169e8cb84edc27a3ec72f
Moritz Freidank <freidanm@informatik.uni-freiburg.de>
2017-09-15 16:41:16 +0200

    interim

--- pysgmcmc/samplers/__init__.py
+++ pysgmcmc/samplers/__init__.py
@@ -1,8 +1,10 @@
 from .sghmc import SGHMCSampler
+from .relativistic_sghmc import RelativisticSGHMCSampler
 from .sgld import SGLDSampler
 
 __all__ = [
     "SGHMCSampler",
+    "RelativisticSGHMCSampler",
     "SGLDSampler",
 
 ]
