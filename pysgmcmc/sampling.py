54cd6eb94109bce633753404cc5d031cdf0d50d5
Moritz Freidank <freidanm@informatik.uni-freiburg.de>
2017-09-18 13:18:45 +0200

    more sanity checking

--- pysgmcmc/sampling.py
+++ pysgmcmc/sampling.py
@@ -497,7 +497,7 @@ class Sampler(Enum):
         >>> sampler = Sampler.get_sampler(Sampler.SGHMC, dtype=tf.float32)
         Traceback (most recent call last):
           ...
-        ValueError: Sampling.Sampler.get_sampler: params was not overwritten as sampler argument in `sampler_args` and does not have any default value in SGHMCSampler.__init__Please pass an explicit value for this parameter.
+        ValueError: sampling.Sampler.get_sampler: params was not overwritten as sampler argument in `sampler_args` and does not have any default value in SGHMCSampler.__init__Please pass an explicit value for this parameter.
 
         If an **optional** argument is not provided as keyword argument,
         the corresponding default value is used.
@@ -510,6 +510,29 @@ class Sampler(Enum):
         >>> with tf.Session() as session: sampler = Sampler.get_sampler(Sampler.SGHMC, session=session, params=params, cost_fun=cost_fun)
         >>> sampler.dtype
         tf.float64
+
+        If a keyword argument that is provided does not represent a valid
+        parameter of the corresponding `sampling_method`, a `ValueError` is
+        raised:
+
+        >>> import tensorflow as tf
+        >>> params = [tf.Variable(0., dtype=tf.float64)]
+        >>> cost_fun = lambda params: tf.reduce_sum(params) # dummy cost function
+        >>> with tf.Session() as session: sampler = Sampler.get_sampler(Sampler.SGHMC, unknown_argument=None, session=session, params=params, cost_fun=cost_fun)
+        Traceback (most recent call last):
+          ...
+        ValueError: sampling.Sampler.get_sampler: 'SGHMCSampler' does not take any parameter with name 'unknown_argument' which was specified as argument to this sampler. Please ensure, that you only specify sampler arguments that fit the corresponding sampling method.
+        For your choice of sampling method ('Sampler.SGHMC'), supported parameters are:
+        -params
+        -cost_fun
+        -batch_generator
+        -epsilon
+        -burn_in_steps
+        -mdecay
+        -scale_grad
+        -session
+        -dtype
+        -seed
 
         """
 
@@ -529,6 +552,33 @@ class Sampler(Enum):
         # look up all initializer parameters with their (potential)
         # default values
         all_sampler_parameters = signature(Sampler.__init__).parameters
+
+        try:
+            undefined_parameter = next(
+                parameter_name for parameter_name in sampler_args
+                if parameter_name not in all_sampler_parameters
+            )
+        except StopIteration:
+            pass
+        else:
+            raise ValueError(
+                "sampling.Sampler.get_sampler: '{sampler_name}' "
+                "does not take any parameter with name '{parameter}' "
+                "which was specified as argument to this sampler. "
+                "Please ensure, that you only specify sampler arguments "
+                "that fit the corresponding sampling method.\n"
+                "For your choice of sampling method ('{sampler}'), supported parameters are:\n"
+                "{valid_parameters}".format(
+                    sampler_name=Sampler.__name__,
+                    sampler=sampling_method,
+                    parameter=undefined_parameter,
+                    valid_parameters="\n".join(
+                        ["-{}".format(parameter_name)
+                         for parameter_name in all_sampler_parameters
+                         if parameter_name != "self"]
+                    )
+                )
+            )
 
         def parameter_value(parameter_name):
             """ Determine the value to assign to the parameter
@@ -555,7 +605,7 @@ class Sampler(Enum):
 
             if parameter_name not in sampler_args and default_value is _empty:
                 raise ValueError(
-                    "Sampling.Sampler.get_sampler: "
+                    "sampling.Sampler.get_sampler: "
                     "{param_name} was not overwritten as sampler argument "
                     "in `sampler_args` and does not have any default value "
                     "in {sampler}.__init__"
