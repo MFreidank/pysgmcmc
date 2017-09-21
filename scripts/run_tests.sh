58c0487b4e2b42b7843806ce207f077deb9152dd
MFreidank <freidankm@yahoo.de>
2017-09-21 11:06:54 +0200

    interim

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-
 fast_tests() {
-    rm -rf .coverage
     make -C pysgmcmc test-notebooks
-    py.test -v --cov=pysgmcmc --doctest-modules --ignore=pysgmcmc/tests/bayesian_neural_network/test_bayesian_neural_network.py
+    py.test -v --doctest-modules --cov=pysgmcmc --ignore=pysgmcmc/tests/bayesian_neural_network/test_bayesian_neural_network.py 
 }
 
 all_tests() {
     py.test -v pysgmcmc/tests/bayesian_neural_network/test_bayesian_neural_network.py
     fast_tests
 }
-
-echo "TRAVIS: $TRAVIS_EVENT_TYPE"
 
 if [[ "$TRAVIS_EVENT_TYPE" == "cron" ]]; then
     # for the daily cron job, run *all* tests (and not just the fast-paced ones)
