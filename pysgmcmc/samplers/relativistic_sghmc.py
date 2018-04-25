edf316d9bab8f967e65b8ca9804fdcda96e76dfb
MFreidank <freidankm@yahoo.de>
2018-04-25 14:12:00 +0200

    deleting lots of stuff, to make place
    This will be strictly pytorch

--- pysgmcmc/samplers/relativistic_sghmc.py
+++
@@ -1,5 +0,0 @@
-from pysgmcmc.optimizers.relativistic_sghmc import RelativisticSGHMC as optimizer_cls
-from pysgmcmc.samplers.base_classes import sampler_from_optimizer
-
-# Automatic conversion of one of our optimizers to a corresponding sampler class.
-RelativisticSGHMCSampler = sampler_from_optimizer(optimizer_cls)
