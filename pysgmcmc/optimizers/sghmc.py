a8ca1299ef3daf4bd0e1112c6b900c38708f1c6d
MFreidank <freidankm@yahoo.de>
2018-05-04 17:42:48 +0200

    towards: sgld
    An initial implementation of sgld ported
    from tensorflow/probability to pytorch.

--- pysgmcmc/optimizers/sghmc.py
+++ pysgmcmc/optimizers/sghmc.py
@@ -1,6 +1,9 @@
+# vim: foldmethod=marker
 import numpy as np
 import torch
 from torch.optim import Optimizer
+
+# XXX: Something is still buggy, maybe try fixing up a test vs theano_mcmc?
 
 
 class SGHMC(Optimizer):
@@ -22,7 +25,6 @@ class SGHMC(Optimizer):
             lr=lr, scale_grad=scale_grad,
             num_burn_in_steps=num_burn_in_steps,
             mdecay=mdecay,
-            num_iterations=0,
             noise=0.
         )
         super().__init__(params, defaults)
@@ -39,36 +41,46 @@ class SGHMC(Optimizer):
                 if parameter.grad is None:
                     continue
 
-                mdecay, lr = group["mdecay"], group["lr"]
-                noise = group["noise"]
-                scale_grad = torch.tensor(group["scale_grad"]).float()
 
                 state = self.state[parameter]
-                gradient = parameter.grad.data
 
-                # State initialization
+                #  State initialization {{{ #
+
                 if len(state) == 0:
                     state["iteration"] = 0
                     state["tau"] = torch.ones_like(parameter)
                     state["g"] = torch.ones_like(parameter)
                     state["v_hat"] = torch.ones_like(parameter)
                     state["momentum"] = torch.zeros_like(parameter)
+                #  }}} State initialization #
 
                 state["iteration"] += 1
+
+                #  Readability {{{ #
+                mdecay, lr = group["mdecay"], group["lr"]
+                noise = group["noise"]
+                scale_grad = torch.tensor(group["scale_grad"]).float()
 
                 tau, g, v_hat = state["tau"], state["g"], state["v_hat"]
                 momentum = state["momentum"]
 
+                gradient = parameter.grad.data
+                #  }}} Readability #
+
                 r_t = 1. / (tau + 1.)
                 minv_t = 1. / torch.sqrt(v_hat)
 
+                #  Burn-in updates {{{ #
                 if state["iteration"] <= group["num_burn_in_steps"]:
                     # Update state
                     tau.add_(1. - tau * (g * g / v_hat))
                     g.add_(-g * r_t + r_t * gradient)
                     v_hat.add_(-v_hat * r_t + r_t * (gradient ** 2))
+                #  }}} Burn-in updates #
 
                 lr_scaled = lr / torch.sqrt(scale_grad)
+
+                #  Draw random sample {{{ #
 
                 noise_scale = (
                     2. * (lr_scaled ** 2) * mdecay * minv_t -
@@ -79,11 +91,15 @@ class SGHMC(Optimizer):
                 sigma = torch.sqrt(torch.clamp(noise_scale, min=1e-16))
 
                 sample_t = torch.normal(mean=torch.tensor(0.)) * sigma
+                #  }}} Draw random sample #
 
+
+                #  SGHMC Update {{{ #
                 momentum.add_(
                     - (lr ** 2) * minv_t * gradient - mdecay * momentum + sample_t
                 )
 
                 parameter.data.add_(momentum)
+                #  }}} SGHMC Update #
 
         return loss
---
+++ pysgmcmc/optimizers/sgld.py
@@ -0,0 +1,82 @@
+import numpy as np
+import torch
+from torch.optim import Optimizer
+
+
+class SGLD(Optimizer):
+    def __init__(self,
+                 params,
+                 lr=1e-3,
+                 precondition_decay_rate=0.95,
+                 num_pseudo_batches=1,
+                 num_burn_in_steps=3000,
+                 diagonal_bias=1e-8)-> None:
+        if lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if num_burn_in_steps < 0:
+            raise ValueError("Invalid num_burn_in_steps: {}".format(num_burn_in_steps))
+
+
+        defaults = dict(
+            lr=lr, precondition_decay_rate=precondition_decay_rate,
+            num_pseudo_batches=num_pseudo_batches,
+            num_burn_in_steps=num_burn_in_steps,
+            diagonal_bias=1e-8,
+        )
+        super().__init__(params, defaults)
+
+    def step(self, closure=None):
+        loss = None
+
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for parameter in group["params"]:
+
+                if parameter.grad is None:
+                    continue
+
+
+                state = self.state[parameter]
+                lr = group["lr"]
+                num_pseudo_batches = group["num_pseudo_batches"]
+                precondition_decay_rate = group["precondition_decay_rate"]
+                gradient = parameter.grad.data
+
+                #  State initialization {{{ #
+
+                if len(state) == 0:
+                    state["iteration"] = 0
+                    state["momentum"] = torch.ones_like(parameter)
+
+                #  }}} State initialization #
+
+                state["iteration"] += 1
+
+                momentum = state["momentum"]
+
+                #  Momentum update {{{ #
+                momentum.add_(
+                    (1.0 - precondition_decay_rate) * ((gradient ** 2) - momentum)
+                )
+                #  }}} Momentum update #
+
+                if state["iteration"] > group["num_burn_in_steps"]:
+                    sigma = 1. / torch.sqrt(torch.tensor(lr))
+                else:
+                    sigma = torch.zeros_like(parameter)
+
+                preconditioner = (
+                    1. / torch.sqrt(momentum + group["diagonal_bias"])
+                )
+
+                scaled_grad = (
+                    0.5 * preconditioner * gradient * num_pseudo_batches +
+                    torch.normal(
+                        mean=torch.zeros_like(gradient),
+                        std=torch.ones_like(gradient)
+                    ) * sigma * torch.sqrt(preconditioner)
+                )
+
+                parameter.data.add_(-lr * scaled_grad)
