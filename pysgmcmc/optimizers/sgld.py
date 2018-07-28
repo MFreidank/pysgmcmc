e75661ffeb4356f74fea7d3aaebf12510db31596
MFreidank <freidankm@yahoo.de>
2018-07-28 10:20:20 +0200

    documentation for sgld

--- pysgmcmc/optimizers/sgld.py
+++ pysgmcmc/optimizers/sgld.py
@@ -2,14 +2,51 @@ import torch
 from torch.optim import Optimizer
 
 
+# Pytorch Port of a previous tensorflow implementation in `tensorflow_probability`:
+# https://github.com/tensorflow/probability/blob/master/tensorflow_probability/g3doc/api_docs/python/tfp/optimizer/StochasticGradientLangevinDynamics.md
 class SGLD(Optimizer):
+    """ Stochastic Gradient Langevin Dynamics Sampler with preconditioning.
+        Optimization variable is viewed as a posterior sample under Stochastic
+        Gradient Langevin Dynamics with noise rescaled in eaach dimension
+        according to RMSProp.
+    """
     def __init__(self,
                  params,
-                 lr=1e-3,
+                 lr=1e-2,
                  precondition_decay_rate=0.95,
                  num_pseudo_batches=1,
                  num_burn_in_steps=3000,
-                 diagonal_bias=1e-8)-> None:
+                 diagonal_bias=1e-8) -> None:
+        """ Set up a SGLD Optimizer.
+
+        Parameters
+        ----------
+        params : TODO
+            Parameters serving as optimization variable.
+        lr : float, optional
+            Base learning rate for this optimizer.
+            Must be tuned to the specific function being minimized.
+            Default: `1e-2`.
+        precondition_decay_rate : float, optional
+            Exponential decay rate of the rescaling of the preconditioner (RMSprop).
+            Should be smaller than but nearly `1` to approximate sampling from the posterior.
+            Default: `0.95`
+        num_pseudo_batches : int, optional
+            Effective number of minibatches in the data set.
+            Trades off noise and prior with the SGD likelihood term.
+            Note: Assumes loss is taken as mean over a minibatch.
+            Otherwise, if the sum was taken, divide this number by the batch size.
+            Default: `1`.
+        num_burn_in_steps : int, optional
+            Number of iterations to collect gradient statistics to update the
+            preconditioner before starting to draw noisy samples.
+            Default: `3000`.
+        diagonal_bias : float, optional
+            Term added to the diagonal of the preconditioner to prevent it from
+            degenerating.
+            Default: `1e-8`.
+
+        """
         if lr < 0.0:
             raise ValueError("Invalid learning rate: {}".format(lr))
         if num_burn_in_steps < 0:
